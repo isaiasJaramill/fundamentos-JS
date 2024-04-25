8663da7ad6aebbdc96b6abf14f870815753525e2
isaiasjaramill <isaiasjaramillo@liceovvh.cl>
2024-04-25 11:20:15 -0400

    ok

---
+++ 06_manipulando_elementos_HTML/01_Ejercicio_tablas/script.js
@@ -0,0 +1,10 @@
+function traerTablas(){
+    let num = parseInt(document.getElementById("inputText").value);
+   
+    let result =`La tabla de ${num} es: <br>`;
+    for(let i = 1;i <= 10;i++){
+        result += `${i} x ${num} = ${i*num}<br>`
+    }
+    document.getElementById("lista").innerHTML = result;
+}
+
